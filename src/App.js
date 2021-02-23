aa903ee54844461fae9dd4864a0c30ccf911117c
Pro <ikouma269@gmail.com>
2021-02-23 17:04:12 +0800

    refactoring our monster rolodex using react hooks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+	const [ monsters, setMonsters ] = useState([]);
+	const [ searchField, setSearchField ] = useState('');
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => setMonsters(users));
+	}, []);
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value);
+	};
+	const filteredMonsters = monsters.filter((monster) =>
+		monster.name.toLowerCase().includes(searchField.toLowerCase())
+	);
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-
-    return (
-      <div className='App'>
-        <h1>Monsters Rolodex</h1>
-        <SearchBox onSearchChange={this.onSearchChange} />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
-
+	return (
+		<div className="App">
+			<h1>Piincher Monsters </h1>
+			<SearchBox onSearchChange={onSearchChange} />
+			<CardList monsters={filteredMonsters} />
+		</div>
+	);
+};
 export default App;
